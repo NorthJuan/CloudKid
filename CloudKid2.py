c5561fa5a8aba7d1ebc9efba9235207035d257fe
NorthJuan <102831530+NorthJuan@users.noreply.github.com>
2022-04-01 14:18:31 -0700

    d

--- CloudKid2.py
+++ CloudKid2.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+
+st.write(df)
+
+st.map(df)
+
 st.title("Mi primer app")
 #st.button("Dale click")
 #st.button("CORRIDOS TUMBADOS")
