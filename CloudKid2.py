332b91ad3f90d0cdbab33ad0eb18694ed7e33340
NorthJuan <102831530+NorthJuan@users.noreply.github.com>
2022-04-01 14:50:42 -0700

    Update CloudKid2.py

--- CloudKid2.py
+++ CloudKid2.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pandas as pd
 
 st.title("Mi primer app")
-df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 
-st.write(df)
+st.markdown(" #titulo ")
+st.markdown(" **este es una vineta** ")
 
-st.map(df)
+#df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
+#st.write(df)
+#st.map(df)
 
 #st.button("Dale click")
 #st.button("CORRIDOS TUMBADOS")
