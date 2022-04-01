170c52d3314af1378859b762306e5c797a8cc4cf
NorthJuan <102831530+NorthJuan@users.noreply.github.com>
2022-04-01 14:55:51 -0700

    Update CloudKid2.py

--- CloudKid2.py
+++ CloudKid2.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 
 st.title("Mi primer app")
+click=st.button("dale click")
+srt.write("el valor de click es: ",click)
 
-st.markdown(" #titulo ")
-st.markdown(" **este es una vineta** ")
+if click==True:
+  st.image("Perrillo.jpg")
+
+#st.markdown(" #titulo ")
+#st.markdown(" **este es una vineta** ")
 
 #df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 #st.write(df)
